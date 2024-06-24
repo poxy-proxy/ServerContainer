a221722b5e41b5c562816649f7fc410e5df18359
poxyproxy <metelkinsp80@gmail.com>
2024-06-24 20:58:20 +0300

    Тесты

---
+++ ServerContainer.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,76 @@
+﻿//using System.Threading.Tasks;
+//using System.Web.Mvc;
+////using LeinNeiro.AspNet.Identity;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.Owin;
+//using Moq;
+//using ServerContainer;
+//using ServerContainer.Controllers;
+//using ServerContainer.Models;
+//using Xunit;
+//namespace ServerContainer.Tests.Controllers
+//{
+//    public class AccountControllerTests
+//    {
+//        [Fact]
+//        public async Task Login_Post_InvalidModelState_ReturnsViewWithModel()
+//        {
+//            // Arrange
+//            var mockUserManager = new Mock<ApplicationUserManager>(Mock.Of<IUserStore<ApplicationUser>>());
+//            var mockSignInManager = new Mock<ApplicationSignInManager>(mockUserManager.Object, Mock.Of<IOwinContext>());
+//            var controller = new AccountController(mockUserManager.Object, mockSignInManager.Object);
+//            controller.ModelState.AddModelError("error", "test error");
+//            var model = new LoginViewModel();
+
+//            // Act
+//            var result = await controller.Login(model, null);
+
+//            // Assert
+//            var viewResult = Assert.IsType<ViewResult>(result);
+//            Assert.Equal(model, viewResult.Model);
+//        }
+
+//        //[Fact]
+//        //public async Task Login_Post_ValidModelState_ReturnsRedirectOnSuccess()
+//        //{
+//        //    // Arrange
+//        //    var model = new LoginViewModel { Email = "Koko", Password = "Koko-1" };
+//        //    var mockUserManager = new Mock<ApplicationUserManager>(Mock.Of<IUserStore<ApplicationUser>>());
+//        //    var mockSignInManager = new Mock<ApplicationSignInManager>(mockUserManager.Object, Mock.Of<IOwinContext>());
+//        //    mockSignInManager.Setup(x => x.PasswordSignInAsync(model.Email, model.Password, false, false))
+//        //        .ReturnsAsync(SignInStatus.Success);
+//        //    var controller = new AccountController(mockUserManager.Object, mockSignInManager.Object);
+
+//        //    // Act
+//        //    var result = await controller.Login(model, "returnUrl");
+
+//        //    // Assert
+//        //    var redirectResult = Assert.IsType<RedirectResult>(result);
+//        //    Assert.Equal("returnUrl", redirectResult.Url);
+//        //}
+
+        
+//        [Fact]
+//        public async Task Login_Post_ValidModelState_ReturnsRedirectOnSuccess()
+//        {
+//            // Arrange
+//            var model = new LoginViewModel { Email = "Koko", Password = "Koko-1" };
+//           // var mockUserStore = Mock.Of<IUserStore<ApplicationUser>>();
+//           // var mockUserManager = new ApplicationUserManager(mockUserStore);
+//           // var mockContext = Mock.Of<IOwinContext>();
+//          //  var mockSignInManager = new ApplicationSignInManager(mockUserManager, mockContext.Authentication);
+
+//            var controller = new AccountController();
+
+//            // Act
+//            var result = await controller.Login(model, "returnUrl");
+
+//            // Assert
+//            var redirectResult = Assert.IsType<RedirectResult>(result);
+//            Assert.Equal("returnUrl", redirectResult.Url);
+//        }
+
+//        // Дополнительные тесты для остальных случаев SignInStatus
+//    }
+//}
---
+++ ServerContainer.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServerContainer;
+using ServerContainer.Controllers;
+using ServerContainer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using Microsoft.Owin;
+namespace ServerContainer.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+      
+
+        private Mock<ControllerContext> _controllerContextMock;
+        private HomeController _controller;
+        private string _name = "testSite";
+        private string _subject = "PHP";
+        private string expectedPath = @"D:\vs projects\ServerContainer\App_Data\Projects\admin_ispi\2faecc8f-091e-4303-a1f1-ea88f39e835a_chinalang\Dockerfile";
+        [TestInitialize]
+        public void Setup()
+        {
+            _controllerContextMock = new Mock<ControllerContext>();
+            _controllerContextMock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns("admin_ispi");
+
+            _controller = new HomeController
+            {
+                ControllerContext = _controllerContextMock.Object
+            };
+        }
+
+        [TestMethod]
+        public async Task RunMySite_ReturnsRedirectToRouteResult_WhenCalled()
+        {
+            // Act
+            var result = await _controller.RunMySite(_name, _subject);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+        }
+
+        [TestMethod]
+        public async Task RunMySite_CopiesDockerfile_WhenSubjectIsPHP()
+        {
+            // Arrange
+            _subject = "NodeJS";
+
+            // Act
+            var result = await _controller.RunMySite(_name, _subject);
+
+            // Assert
+            Assert.IsTrue(File.Exists(expectedPath), @"D:\vs projects\ServerContainer\App_Data\Projects\admin_ispi\2faecc8f-091e-4303-a1f1-ea88f39e835a_chinalang\Dockerfile");
+        }
+
+        [TestMethod]
+        public void Index_ReturnsNotNull()
+        {
+            // Arrange
+           // var controller = new YourController();  // Замените на название вашего контроллера
+
+           
+
+            // Act
+            var result = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Tasks_Returns()
+        {
+            // Arrange
+            // var controller = new YourController();  // Замените на название вашего контроллера
+
+
+
+            // Act
+            var result = _controller.Tasks() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void MySite_Returns()
+        {
+            // Arrange
+            // var controller = new YourController();  // Замените на название вашего контроллера
+
+
+
+            // Act
+            var result = _controller.MySite() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DownloadTask()
+        {
+            // Arrange
+            // var controller = new YourController();  // Замените на название вашего контроллера
+
+
+
+            // Act
+            var result = _controller.DownloadTask(@"Задание картинка.jpg") ;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
